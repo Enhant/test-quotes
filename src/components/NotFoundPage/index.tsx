e1a1465bb42bf5f604971fc5a478dc46383f312e
Daniel <daniel.kuzin.sn@gmail.com>
2020-11-16 16:15:05 +0200

    remark: eslint changes

--- src/components/NotFoundPage/index.tsx
+++ src/components/NotFoundPage/index.tsx
@@ -4,20 +4,20 @@ import { NavLink } from 'react-router-dom';
 import Container from 'components/Container';
 
 export default class NotFound extends React.PureComponent {
-  render() {
-    return (
-      <Container>
-        <br />
-        <h1>404</h1>
-        <h3>Location Unknown</h3>
-        <p>Страница не существует</p>
-        <p>
-          Вы можете вернутся к приложению{' '}
-          <NavLink exact to="/">
-            здесь
-          </NavLink>
-        </p>
-      </Container>
-    );
-  }
+    render() {
+        return (
+            <Container>
+                <br />
+                <h1>404</h1>
+                <h3>Location Unknown</h3>
+                <p>Страница не существует</p>
+                <p>
+                    Вы можете вернутся к приложению{' '}
+                    <NavLink exact to="/">
+                        здесь
+                    </NavLink>
+                </p>
+            </Container>
+        );
+    }
 }
--- src/containers/Quotes/index.tsx
+++ src/containers/Quotes/index.tsx
@@ -9,58 +9,58 @@ import LoadingIndicator from 'components/LoadingIndicator';
 import { IState } from 'types';
 
 const useStyles = createUseStyles({
-  button: {
-    background: 'rgba(90, 206, 249, 1)',
-    padding: '10px 20px',
-    width: 100,
-    display: 'fixed',
-    top: 10,
-    left: 10,
-  },
+    button: {
+        background: 'rgba(90, 206, 249, 1)',
+        padding: '10px 20px',
+        width: 100,
+        display: 'fixed',
+        top: 10,
+        left: 10,
+    },
 });
 
 const Quotes: React.FC = () => {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  const dispatch = useDispatch();
-  const quotes = useSelector((state: { quotes: IState }) => state.quotes.data);
-  const error = useSelector((state: { quotes: IState }) => state.quotes.error);
+    const dispatch = useDispatch();
+    const quotes = useSelector((state: { quotes: IState }) => state.quotes.data);
+    const error = useSelector((state: { quotes: IState }) => state.quotes.error);
 
-  useInterval(() => {
-    dispatch(fetchTicket());
-  });
+    useInterval(() => {
+        dispatch(fetchTicket());
+    });
 
-  if (quotes.length === 0) {
-    return <LoadingIndicator />;
-  }
+    if (quotes.length === 0) {
+        return <LoadingIndicator />;
+    }
 
-  const columns = [
-    {
-      name: 'Name',
-      cell: 'name',
-    },
-    {
-      name: 'last',
-      cell: 'last',
-    },
-    {
-      name: 'highestBid',
-      cell: 'highestBid',
-    },
-    {
-      name: 'percentChange',
-      cell: 'percentChange',
-    },
-  ];
+    const columns = [
+        {
+            name: 'Name',
+            cell: 'name',
+        },
+        {
+            name: 'last',
+            cell: 'last',
+        },
+        {
+            name: 'highestBid',
+            cell: 'highestBid',
+        },
+        {
+            name: 'percentChange',
+            cell: 'percentChange',
+        },
+    ];
 
-  return (
-    <>
-      <Button to="/" className={classes.button}>
-        Назад к "О приложении"
-      </Button>
-      <QuotesTable data={quotes} columns={columns} error={error} />
-    </>
-  );
+    return (
+        <>
+            <Button to="/" className={classes.button}>
+                Назад к "О приложении"
+            </Button>
+            <QuotesTable data={quotes} columns={columns} error={error} />
+        </>
+    );
 };
 
 export default Quotes;
